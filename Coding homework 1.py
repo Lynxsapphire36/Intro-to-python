0d3dac74e2265499021d54f450e1caa30056443b
Lynxsapphire36 <122318797+Lynxsapphire36@users.noreply.github.com>
2023-01-25 20:55:10 -0600

    Finished homework 1

---
+++ Coding homework 1.py
@@ -0,0 +1,44 @@
+Integervar = 1
+Floatvar = 1.2
+singlevar = 'hi'
+apostrophe = "Isn\'t life great"
+
+print(Integervar)
+print(Floatvar)
+print(singlevar)
+print(apostrophe)
+
+# Part 2
+
+floor = 11//3
+modulo = 11%3
+power = 2**2
+root = 4**1/2
+
+# Part 3
+
+numbers = "0123456789"
+
+print(numbers[0:1])
+print(numbers[9:10])
+print(numbers[0:10:2])
+print(numbers[::-1])
+print(numbers[::-2])
+
+# Part 4
+
+abc = "The quick brown fox jumped over the lazy dog"
+
+print(abc.upper())
+print(abc.lower())
+print(abc.split())
+
+# Part 5
+
+babymath = ["one","two","three"]
+
+print(babymath[1])
+babymath[2]={"key1":4,"key2":"four"}
+print(babymath)
+print(babymath[2]["key1"]*2)
+print(babymath[2]["key2"].upper())
